e42b0154e5e242dca176d4736947961fbe8a86ae
Yoana <yoanabarbie@icloud.com>
2024-06-12 15:46:34 +0200

    feat:added functions

---
+++ funciones.js
@@ -0,0 +1,55 @@
+//RETO 1
+
+// let celsius = prompt("Indica la temperatura en celsius")
+
+// const convertToFarenheit = (celsius) => { 
+//     return (celsius * 9/5) + 32; 
+// }
+
+// console.log(convertToFarenheit(celsius));
+
+// RETO 2
+
+// const esPar = (num) => {
+//     if (num % 2 == 0) {
+//         console.log(`El numero ${numero} es par`);
+//     }
+//     return console.log(`El numero ${num} no es par!`)
+// }
+
+// RETO 3
+
+// let num = prompt("Indica un numero");
+
+// const esPrimo = (num) => {
+//     for (let i = 2, max = Math.sqrt(num); i <= max; i++) {
+//         if (num % i === 0) {
+//             return console.log(`El número ${num} no es primo!`);
+//         }
+//     }
+//     return console.log(`El número ${num} es primo`);
+// }
+
+// esPrimo(num);
+
+// RETO 4
+
+// let letras = ["T","R","W","A","G","M","Y","F","P","D","X",
+// "B","N","J","Z","S","Q","V","H","L","C","K","E"];
+
+// let numeros = prompt("Indica los numeros de tu DNI");
+
+// const comprobarDni = (numeros) => {
+//     if (num > 0 && num <= letters.length) {
+//         console.log(`La letra en la posición ${num} es: ${letters[num-1]}`);
+//     } else {
+//         console.log("Número fuera de rango.");
+//     }
+// }
+
+// RETO 5 
+
+function Parar(){
+    
+}
+
